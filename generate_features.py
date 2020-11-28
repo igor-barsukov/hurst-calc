67b66b1b4a64da5d5d475e0204afb3b475382172
igba0714 <igor.barsukov@NetCracker.com>
2020-11-28 14:26:41 +0300

    modified logic for get_flags + refactoring

--- generate_features.py
+++ generate_features.py
@@ -11,10 +11,13 @@ class Connection:
         self.a_port = a_port
         self.b_addr = b_addr
         self.b_port = b_port
+        self.key = a_addr + '_' + str(a_port) + '_' + b_addr + '_' + str(b_port)        
+        self.reversed_key = b_addr + '_' + str(b_port) + '_' + a_addr + '_' + str(a_port)
         self.a_bytes = 0
         self.b_bytes = 0
-        self.key = a_addr + '_' + str(a_port) + '_' + b_addr + '_' + str(b_port)
-        self.reversed_key = b_addr + '_' + str(b_port) + '_' + a_addr + '_' + str(a_port)
+        self.a_flags = []
+        self.b_flags = []
+
 
     def setABytes(self, bytes):
         self.a_bytes += bytes
@@ -22,10 +25,87 @@ class Connection:
     def setBBytes(self, bytes):
         self.b_bytes += bytes
 
+    def setAFlags(self, packet_flags):
+        self.a_flags.append(packet_flags)
+
+    def setBFlags(self, packet_flags):
+        self.b_flags.append(packet_flags)
+
     def __str__(self):
-        return f'Connection: A host - {self.a_addr}:{self.a_port} with {self.a_bytes} bytes, B host - {self.b_addr}:{self.b_port} with {self.b_bytes} bytes'
+        return (f'Connection: A host - {self.a_addr}:{self.a_port} with {self.a_bytes} bytes - flags: {self.a_flags},\n'
+                            f'B host - {self.b_addr}:{self.b_port} with {self.b_bytes} bytes - flags: {self.b_flags}')
      
 
+'''
+Helper methods
+'''
+
+def get_tcp_connections(pcap):
+    tcp_session_keys = set()
+    syn_fin_packets = {}
+    processedKeys = []
+    for ts, buf in pcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        tcp_response = detect_tcp(eth)
+        if(tcp_response is not None):
+            key = generate_tcp_connection_key(tcp_response)
+            if (key not in syn_fin_packets):
+                syn_fin_packets[key] = ts
+            tcp_session_keys.add(key)
+    with open('syn_fin_packets1.txt', 'w') as f:
+        for k, v in syn_fin_packets.items():
+            f.write(str(k) + ' >>> '+ str(v) + '\n')
+    return syn_fin_packets
+
+
+def generate_tcp_connection_key(tcp_resp):
+    tcp = tcp_resp[0]
+    s_ip = tcp_resp[1]
+    d_ip = tcp_resp[2]
+    s_port = tcp.sport
+    d_port = tcp.dport
+    key =  s_ip + '_' + d_ip + '_' + str(s_port) + '_' + str(d_port)
+    return key
+
+
+def generate_tcp_connection_obj(tcp_resp):
+    tcp = tcp_resp[0]
+    s_ip = tcp_resp[1]
+    d_ip = tcp_resp[2]
+    s_port = tcp.sport
+    d_port = tcp.dport
+    conn = Connection(a_addr=s_ip, a_port=s_port, b_addr=d_ip, b_port=d_port)
+    return conn
+
+
+def if_key_exists(key):
+    if(synFinPackets.get(key) is not None):
+        return True
+    else:
+        return False
+
+
+def detect_tcp(eth):
+    if( len(eth.data) > 0 and (eth.type == dpkt.ethernet.ETH_TYPE_IP) ):
+        ip = eth.data
+        # print ('s_ip - {}, d_ip - {}'.format(inet_to_str(ip.src), inet_to_str(ip.dst)))
+        if( len(ip.data) > 0 and (ip.p == dpkt.ip.IP_PROTO_TCP) ):
+            return ip.data, inet_to_str(ip.src), inet_to_str(ip.dst)
+    return
+
+
+def inet_to_str(inet):
+    try:
+        return socket.inet_ntop(socket.AF_INET, inet)
+    except ValueError:
+        return socket.inet_ntop(socket.AF_INET6, inet)
+
+
+'''
+Methods for calculate specific characteristics of traffic
+'''
+
+# obsolete
 def get_duration(pcap):
     print('get_duration')
     tcp_session_keys = set()
@@ -59,101 +139,6 @@ def get_duration(pcap):
     #     for k, v in syn_fin_packets.items():
     #         f.write(str(k) + ' >>> '+ str(v) + '\n\n')
 
-def get_tcp_connections(pcap):
-    tcp_session_keys = set()
-    syn_fin_packets = {}
-    processedKeys = []
-    for ts, buf in pcap:
-        eth = dpkt.ethernet.Ethernet(buf)
-        tcp_response = detect_tcp(eth)
-        if(tcp_response is not None):
-            key = generate_tcp_connection_key(tcp_response)
-            if (key not in syn_fin_packets):
-                syn_fin_packets[key] = ts
-            tcp_session_keys.add(key)
-    with open('syn_fin_packets1.txt', 'w') as f:
-        for k, v in syn_fin_packets.items():
-            f.write(str(k) + ' >>> '+ str(v) + '\n')
-    return syn_fin_packets
-
-def generate_tcp_connection_key(tcp_resp):
-    tcp = tcp_resp[0]
-    s_ip = tcp_resp[1]
-    d_ip = tcp_resp[2]
-    s_port = tcp.sport
-    d_port = tcp.dport
-    key =  s_ip + '_' + d_ip + '_' + str(s_port) + '_' + str(d_port)
-    return key
-
-def generate_tcp_connection_obj(tcp_resp):
-    tcp = tcp_resp[0]
-    s_ip = tcp_resp[1]
-    d_ip = tcp_resp[2]
-    s_port = tcp.sport
-    d_port = tcp.dport
-    conn = Connection(a_addr=s_ip, a_port=s_port, b_addr=d_ip, b_port=d_port)
-    return conn
-
-# obsolete
-def get_duration1(pcap):
-    # идея - не проверять флаги, а просто сканировать для каждого соединения, находить 1й и последний и по разнице ts выводить duration
-    print('get_duration')
-    syn_fin_packets = get_tcp_connections(pcap)
-    print('syn_fin_packets size - ', len(syn_fin_packets))
-
-    infile.seek(0)  # to reset cursor and read file again
-    pcap = dpkt.pcap.Reader(infile)
-    syn_fin_packets_2 = {}
-    for ts, buf in pcap:
-        # print('here1 - ', len(buf))
-        eth = dpkt.ethernet.Ethernet(buf)
-        tcp_response = detect_tcp(eth)
-        if(tcp_response is not None):
-            tcp = tcp_response[0]
-            s_ip = tcp_response[1]
-            d_ip = tcp_response[2]
-            s_port = tcp.sport
-            d_port = tcp.dport
-            scanned_key =  s_ip + '_' + d_ip + '_' + str(s_port) + '_' + str(d_port)
-            # print('scanned_key - ', scanned_key)
-            for key, val in syn_fin_packets.items():
-                last_ts_val = 0.0
-                if (scanned_key == key):
-                    # print('matched key  - ', key)
-                    last_ts_val = ts
-            print('here')
-            dur = last_ts_val - val
-            syn_fin_packets_2[key] = dur
-
-    # for key, val in syn_fin_packets.items():
-    #     last_ts_val = 0.0
-    #     # print('key        - ', key)
-    #     # f.write('key        - ' + key + '\n')
-    #     # print('pcap type - ', type(pcap))
-    #     infile = open('D:/Aspirantura/traffic/moodle_2020/testfile.2020-06-07.%H.%M.%S.pcap', 'rb')
-    #     pcap1 = dpkt.pcap.Reader(infile)
-    #     for ts, buf in pcap1:
-    #         # print('here')
-    #         # f.write('here' + '\n')
-    #         eth = dpkt.ethernet.Ethernet(buf)
-    #         tcp_response = detect_tcp(eth)
-    #         if(tcp_response is not None):
-    #             tcp = tcp_response[0]
-    #             s_ip = tcp_response[1]
-    #             d_ip = tcp_response[2]
-    #             s_port = tcp.sport
-    #             d_port = tcp.dport
-    #             scanned_key =  s_ip + '_' + d_ip + '_' + str(s_port) + '_' + str(d_port)
-    #             # print('matched key - ', scanned_key)
-    #             # f.write('scanned_key  -' + scanned_key + '\n')
-    #             if (scanned_key == key):
-    #                 # f.write('matched key  -' + scanned_key + '\n')
-    #                 last_ts_val = ts
-    #     dur = last_ts_val - val
-    #     syn_fin_packets[key] = dur
-    with open('syn_fin_packets3.txt', 'w') as f:
-        for k, v in syn_fin_packets_2.items():
-            f.write(str(k) + ' >>> '+ str(v) + '\n')
 
 def get_duration2(pcap):
     print('get_duration2')
@@ -179,7 +164,7 @@ def get_duration2(pcap):
     conn_durations_new_dict = {}
     for k, v in conn_durations_dict.items():
         if(len(v) > 1):
-            conn_durations_new_dict[k] = v[1] - v[0]
+            conn_durations_new_dict[k] = v[1] - v[0] # calculate duration as difference between last and 1st packet
         else:
             conn_durations_new_dict[k] = 0.0  # in case if only one packet for connection is present
     print("conn_durations_new_dict size - ", len(conn_durations_new_dict))
@@ -194,7 +179,7 @@ def get_protocol(pcap):
 def get_service(pcap):
     print('get_service')
 
-def get_flag(pcap):
+def get_flags(pcap):
     '''
     11 флагов
     SF - normal establishment and termination.
@@ -222,7 +207,7 @@ def get_flag(pcap):
           <- FIN
     ACK ->
     '''
-    print('get_flag')
+    print('get_flags')
     conn_flags_dict = {}
     for ts, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
@@ -240,16 +225,16 @@ def get_flag(pcap):
             cwr_flag = ( tcp.flags & dpkt.tcp.TH_CWR ) != 0
 
             flags = (
-            ( "C" if cwr_flag else " " ) +
-            ( "E" if ece_flag else " " ) +
-            ( "U" if urg_flag else " " ) +
-            ( "A" if ack_flag else " " ) +
-            ( "P" if psh_flag else " " ) +
-            ( "R" if rst_flag else " " ) +
-            ( "S" if syn_flag else " " ) +
-            ( "F" if fin_flag else " " ) )
+            ( "C" if cwr_flag else "" ) +
+            ( "E" if ece_flag else "" ) +
+            ( "U" if urg_flag else "" ) +
+            ( "A" if ack_flag else "" ) +
+            ( "P" if psh_flag else "" ) +
+            ( "R" if rst_flag else "" ) +
+            ( "S" if syn_flag else "" ) +
+            ( "F" if fin_flag else "" ) )
 
-            insert_or_update_dict(conn_flags_dict, key, flags + " ", list_val = True)
+            insert_or_update_dict(conn_flags_dict, key, flags, list_val = True)
 
             if syn_flag and not ack_flag:
                 # connection establishing 1 - client send SYN to server ->
@@ -283,6 +268,87 @@ def get_flag(pcap):
         for k, v in conn_flags_dict.items():
             f.write(str(k) + ' >>> ' + str(v) + '\n')
 
+def get_flags1(pcap):
+    print("get_flags1")
+    conn_flags_dict = {}
+    for ts, buf in pcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        tcp_response = detect_tcp(eth)
+        if(tcp_response is not None):
+            conn = generate_tcp_connection_obj(tcp_response)
+            tcp = tcp_response[0]
+            fin_flag = ( tcp.flags & dpkt.tcp.TH_FIN ) != 0
+            syn_flag = ( tcp.flags & dpkt.tcp.TH_SYN ) != 0
+            rst_flag = ( tcp.flags & dpkt.tcp.TH_RST ) != 0
+            psh_flag = ( tcp.flags & dpkt.tcp.TH_PUSH) != 0
+            ack_flag = ( tcp.flags & dpkt.tcp.TH_ACK ) != 0
+            urg_flag = ( tcp.flags & dpkt.tcp.TH_URG ) != 0
+            ece_flag = ( tcp.flags & dpkt.tcp.TH_ECE ) != 0
+            cwr_flag = ( tcp.flags & dpkt.tcp.TH_CWR ) != 0
+
+            flags = (
+            ( "C" if cwr_flag else "" ) +
+            ( "E" if ece_flag else "" ) +
+            ( "U" if urg_flag else "" ) +
+            ( "A" if ack_flag else "" ) +
+            ( "P" if psh_flag else "" ) +
+            ( "R" if rst_flag else "" ) +
+            ( "S" if syn_flag else "" ) +
+            ( "F" if fin_flag else "" ) )
+
+            if (conn.key in conn_flags_dict):
+                stored_conn = conn_flags_dict[conn.key]
+                stored_conn.setAFlags(flags)
+                conn_flags_dict[conn.key] = stored_conn
+            elif (conn.reversed_key in conn_flags_dict):
+                stored_conn = conn_flags_dict[conn.reversed_key]
+                stored_conn.setBFlags(flags)
+                conn_flags_dict[conn.reversed_key] = stored_conn
+            else:
+                conn.setAFlags(flags)
+                conn_flags_dict[conn.key] = conn
+
+    print("conn_flags_dict size - ", len(conn_flags_dict))
+    # with open('get_flags1.txt',mode='w') as f:
+    #     for k, v in conn_flags_dict.items():
+    #         f.write(str(v) + '\n')
+    analyze_flags(conn_flags_dict)
+
+def analyze_flags(conn_flags_dict):
+    conn_final_flags_dict = {}
+    for key, conn in conn_flags_dict.items():
+        if all(x in conn.a_flags for x in ['S','A','AF']) and all(x in conn.b_flags for x in ['AS','A','AF']):
+            conn_final_flags_dict[key] = 'SF'
+        elif ('S' in conn.a_flags) and ('AS' in conn.b_flags) and ('R' in conn.a_flags):
+            conn_final_flags_dict[key] = 'REJ' # possibly wrong conditions
+        elif ('S' in conn.a_flags) and ('AS' not in conn.b_flags):
+            conn_final_flags_dict[key] = 'S0'
+        elif all(x in conn.a_flags for x in ['S','A']) and ('AS' in conn.b_flags) and ('AF' not in conn.a_flags) and ('AF' not in conn.b_flags):
+            conn_final_flags_dict[key] = 'S1'
+        elif all(x in conn.a_flags for x in ['S','A']) and ('AS' in conn.b_flags) and ('AF' in conn.a_flags) and ('AF' not in conn.b_flags):
+            conn_final_flags_dict[key] = 'S2'
+        elif all(x in conn.a_flags for x in ['S','A']) and ('AS' in conn.b_flags) and ('AF' not in conn.a_flags) and ('AF' in conn.b_flags):
+            conn_final_flags_dict[key] = 'S3'
+        elif all(x in conn.a_flags for x in ['S','A']) and ('AS' in conn.b_flags) and ('R' in conn.a_flags) and ('R' not in conn.b_flags):
+            conn_final_flags_dict[key] = 'RSTO' # possibly wrong conditions
+        elif all(x in conn.a_flags for x in ['S','A']) and ('AS' in conn.b_flags) and ('R' not in conn.a_flags) and ('R' in conn.b_flags):
+            conn_final_flags_dict[key] = 'RSTR' # possibly wrong conditions
+        elif all(x not in conn.a_flags for x in ['S','AF']) and all(x not in conn.b_flags for x in ['AS','AF']):
+            conn_final_flags_dict[key] = 'OTH'
+        elif all(x in conn.a_flags for x in ['S','R']) and ('AS' not in conn.b_flags):
+            conn_final_flags_dict[key] = 'RSTOS0' # possibly wrong conditions
+        elif False:
+            # 'SH'
+            pass
+        else:
+            conn_final_flags_dict[key] = 'without flag'
+
+    print("conn_final_flags_dict size - ", len(conn_final_flags_dict))
+    with open('analyze_flags.txt',mode='w') as f:
+        for k, v in conn_final_flags_dict.items():
+            f.write(str(k) + ' >>> ' + str(v) + '\n')
+
+
 def get_urgents(pcap):
     print('get_urgents')
     conn_urgents_dict = {}
@@ -300,7 +366,7 @@ def get_urgents(pcap):
     print("conn_urgents_dict size - ", len(conn_urgents_dict))
     with open('get_urgents.txt',mode='w') as f:
         for k, v in conn_urgents_dict.items():
-            f.write(str(k) + ' >>> ' + str(v) + '\n')
+            f.write(str(v) + '\n')
 
 
 def insert_or_update_dict(conn_dict, key, val, increment = False, list_val = False):
@@ -314,35 +380,8 @@ def insert_or_update_dict(conn_dict, key, val, increment = False, list_val = Fal
             fetched_val += 1    
         conn_dict[key] = fetched_val
 
-# сейчас считывает полное число байт в обе стороны, совпадает с полем Bytes в шарке
-# как определить кол-во байт в разные стороны? по флагам?
-# syn(1),ack(0) - от клиента, syn(1),ack(1) - от сервера, syn(0),ack(1) - от клиента
-# ("All packets after the initial SYN packet sent by the client should have ACK flag set.")
-def get_src_bytes(pcap):
-    print('get_src_bytes')
-    syn_fin_packets = get_tcp_connections(pcap)
-    conn_bytes_dict = {}
-    print('syn_fin_packets size - ', len(syn_fin_packets))
-    for key in syn_fin_packets.keys():
-        infile.seek(0)  # to reset cursor and read file again
-        pcap = dpkt.pcap.Reader(infile)
-        for ts, buf in pcap:
-            eth = dpkt.ethernet.Ethernet(buf)
-            tcp_response = detect_tcp(eth)
-            if(tcp_response is not None):
-                scanned_key = generate_tcp_connection_key(tcp_response)
-                if (scanned_key == key):
-                    if (key not in conn_bytes_dict):
-                        conn_bytes_dict[scanned_key] = len(buf)
-                    else:
-                        bytes_count = conn_bytes_dict[scanned_key]
-                        bytes_count = bytes_count + len(buf)
-                        conn_bytes_dict[scanned_key] = bytes_count
-    print('conn_bytes_dict size - ', len(conn_bytes_dict))
-    # with open('get_src_bytes.txt',mode='w') as f:
-    #     for k, v in conn_bytes_dict.items():
-    #         f.write(str(k) + ' >>> ' + str(v) + '\n')
 
+# obsolete (считывает полное число байт в обе стороны, совпадает с полем Bytes в шарке)
 def get_src_bytes1(pcap):
     conn_bytes_dict = {}
     for ts, buf in pcap:
@@ -397,35 +436,15 @@ def get_src_bytes2(pcap):
 def get_dst_bytes(pcap):
     print('get_dst_bytes')
 
-def if_key_exists(key):
-    if(synFinPackets.get(key) is not None):
-        return True
-    else:
-        return False
-
-
-def detect_tcp(eth):
-    if( len(eth.data) > 0 and (eth.type == dpkt.ethernet.ETH_TYPE_IP) ):
-        ip = eth.data
-        # print ('s_ip - {}, d_ip - {}'.format(inet_to_str(ip.src), inet_to_str(ip.dst)))
-        if( len(ip.data) > 0 and (ip.p == dpkt.ip.IP_PROTO_TCP) ):
-            return ip.data, inet_to_str(ip.src), inet_to_str(ip.dst)
-    return
-
-def inet_to_str(inet):
-    try:
-        return socket.inet_ntop(socket.AF_INET, inet)
-    except ValueError:
-        return socket.inet_ntop(socket.AF_INET6, inet)
 
 def main():
     pcap = dpkt.pcap.Reader(infile)
-    # get_duration1(pcap)
     # get_src_bytes1(pcap)
     # get_duration2(pcap)
-    # get_flag(pcap)
+    # get_flags(pcap)
     # get_urgents(pcap)
-    get_src_bytes2(pcap)
+    # get_src_bytes2(pcap)
+    get_flags1(pcap)
 
 if __name__ == '__main__':
     main()
